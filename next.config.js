99e033cd5254f691a6249ea03b755ec366c4b4cb
Ilyas Aouad <aouadilyas@gmail.com>
2025-11-22 22:53:19 +0100

    This to deploy vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
+        // force Next.js to use Webpack instead of Turbopack
+        useWebpackBuildFallback: true,
+
+        // needed for pino & thread-stream
         serverComponentsExternalPackages: ["pino", "thread-stream"],
     },
 };
